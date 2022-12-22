50c01b68e2548b8261bdab3115a641231c523004
C0okiedestroyer <ng.patrick54@gmail.com>
2022-12-22 17:15:11 +0100

    started with memorycard prototype (nonfuntinal)

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
-<template>
-  <div class="app">test</div>
-</template>
-
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
+import MemoryCard from "./components/MemoryCard.vue";
 export default defineComponent({
   name: "App",
-  components: {},
+  components: { MemoryCard },
 });
+const cards = ref([
+  { id: 1, text: "Hallo" },
+  { id: 2, text: "Test" },
+  { id: 3, text: "Yay" },
+]);
 </script>
+
+<template>
+  <div class="app">test</div>
+  <MemoryCard v-for="card in cards" :key="card.id" :text="card.text">
+  </MemoryCard>
+</template>
---
+++ src/components/MemoryCard.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+export default {
+  props: ["text"],
+  name: "MemoryCard",
+  setup(props) {
+    console.log(props.text);
+  },
+};
+</script>
+
+<template>
+  <p>{{ text }}</p>
+</template>
+<style scoped>
+#MemoryCard {
+  width: 400px;
+  height: 100px;
+}
+</style>
