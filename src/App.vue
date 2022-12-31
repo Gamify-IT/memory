f72ccaafcee05ba6f50efff816fc4daf170778bd
C0okiedestroyer <ng.patrick54@gmail.com>
2022-12-31 22:20:56 +0100

    started flip animation

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,12 @@
 <script lang="ts">
-import { defineComponent, ref } from "vue";
+import { defineComponent } from "vue";
 import MemoryCard from "./components/MemoryCard.vue";
 export default defineComponent({
   name: "App",
   components: { MemoryCard },
 });
-const cards = ref([
-  { id: 1, text: "Hallo" },
-  { id: 2, text: "Test" },
-  { id: 3, text: "Yay" },
-]);
 </script>
 
 <template>
-  <div class="app">test</div>
-  <MemoryCard v-for="card in cards" :key="card.id" :text="card.text">
-  </MemoryCard>
+  <div class="app"><MemoryCard /></div>
 </template>
--- src/components/MemoryCard.vue
+++ src/components/MemoryCard.vue
@@ -1,19 +1,51 @@
 <script lang="ts">
 export default {
-  props: ["text"],
-  name: "MemoryCard",
-  setup(props) {
-    console.log(props.text);
+  data() {
+    return {};
+  },
+  methods: {
+    flipCard() {
+      let x: HTMLElement | null = document.getElementById("card"); //<HTMLDivElement>this.$refs.card;
+      if (x != null) x.classList.toggle("flip");
+    },
   },
 };
 </script>
 
 <template>
-  <p>{{ text }}</p>
+  <div class="MemoryCard" id="card" @click="flipCard()">
+    <div class="front">front</div>
+    <div class="back">back</div>
+  </div>
 </template>
+
 <style scoped>
-#MemoryCard {
-  width: 400px;
-  height: 100px;
+.MemoryCard {
+  width: 100px; /*calc(25%-10px)*/
+  height: 100px; /*calc(25%-10px)*/
+  cursor: pointer;
+  transform: scale(1);
+  margin: 5px;
+  position: relative;
+  transform-style: preserve-3d;
+}
+.MemoryCard:active {
+  transform: scale(0.95);
+  transition: transform 0.25s;
+}
+.MemoryCard.flip {
+  transform: rotateY(180deg);
+}
+.front,
+.back {
+  width: 100%;
+  height: 100%;
+  background: grey;
+  position: absolute;
+  text-align: center;
+  backface-visibility: hidden;
+}
+.front {
+  transform: rotateY(180deg);
 }
 </style>
