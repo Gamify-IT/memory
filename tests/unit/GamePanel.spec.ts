efb8339990ecd77d9a88ba433bbb4c377841e8f1
C0okiedestroyer <ng.patrick54@gmail.com>
2023-01-31 22:23:15 +0100

    added more tests

---
+++ tests/unit/GamePanel.spec.ts
@@ -0,0 +1,56 @@
+import { mount, VueWrapper } from "@vue/test-utils";
+import GamePanel from "@/components/GamePanel.vue";
+import { CardData } from "@/types/data-models";
+import ContentModal from "@/components/ContentModal.vue";
+import MemoryCard from "@/components/MemoryCard.vue";
+import PairItem from "@/components/PairItem.vue";
+import { PropType } from "vue";
+
+
+  describe("GamePanel.vue", () => {
+    let wrapper: VueWrapper;
+    let card;
+    let cards;
+    let button;
+    let card1;
+    let card2;
+    beforeEach(() => {
+        wrapper = mount(GamePanel);
+        jest.useFakeTimers();
+    });
+
+    test("test", () => {
+        expect(wrapper.exists()).toBe(true);
+    });
+
+    test("GamePanel gets displayed", () => {
+        expect(wrapper.find("#game-panel").exists()).toBe(true);
+    });
+
+    test("opens and closes ContentModal correctly", async () => {
+      card = wrapper.findComponent(MemoryCard);
+      expect(card.exists()).toBe(true);
+      button = card.find("#detail-view");
+      expect(button.exists()).toBe(true);
+      button.trigger("click");
+      await wrapper.vm.$nextTick();
+      expect(wrapper.findComponent(ContentModal).exists()).toBe(true);
+      wrapper.find("#close-button").trigger("click");
+      await wrapper.vm.$nextTick();
+      expect(wrapper.findComponent(ContentModal).exists()).toBe(false);
+    });
+
+    test("card reveal procedure", async () => {
+     cards = wrapper.findAllComponents(MemoryCard);
+     expect(cards.length).toBe(12);
+     card1 = cards[0];
+     card2 = cards[1];
+     card1.trigger("click");
+     await wrapper.vm.$nextTick();
+     card2.trigger("click");
+     await wrapper.vm.$nextTick();
+     jest.advanceTimersByTime(1000);
+     await wrapper.vm.$nextTick();
+     expect(wrapper.findAllComponents(PairItem).length).toBe(1);
+    })
+  });
--- tests/unit/StartGameScene.spec.ts
+++ tests/unit/StartGameScene.spec.ts
@@ -1,10 +1,34 @@
 import StartGameScene from "@/components/StartGameScene.vue";
 import { mount } from "@vue/test-utils";
+import router from "@/router/index";
 
-describe("StartGameScene", () => {
-  const wrapper = mount(StartGameScene);
+describe("StartGameScreen.vue", () => {
+  test("correct route to Singleplayer", async () =>{
+    const push = jest.spyOn(router, "push");
+    const wrapper = mount(StartGameScene, {
+      global: {
+        plugins: [router],
+      },
+     })
+   router.push("/singleplayer")
+   await router.isReady();
+   await wrapper.find("button").trigger("click");
+   expect(push).toHaveBeenCalledWith({path: "/singleplayer"});
 
-  test("tets", () => {
-    expect(wrapper.exists()).toBe(true);
-  });
-});
+   });
+   /*test("correct route to Mulitplayer", async () =>{
+    const push = jest.spyOn(router, "push");
+    const wrapper = mount(StartGameScene, {
+      global: {
+        plugins: [router],
+      },
+     })
+   router.push("/multiplayer")
+   await router.isReady();
+   await wrapper.find("button").trigger("click");
+   expect(push).toHaveBeenCalledWith({path: "/multiplayer"});
+   });*/
+
+})
+  
+  
