4e7ebd118c1a61d56984844aef7c33cb82ae9788
katyaganina <89980843+katyaganina@users.noreply.github.com>
2024-07-21 22:48:13 +0200

    Update StartGameScene.vue

--- src/components/StartGameScene.vue
+++ src/components/StartGameScene.vue
@@ -18,11 +18,11 @@
 
 <script setup lang="ts">
 import { useRoute, useRouter } from "vue-router";
-import clickSoundSource from '/src/assets/music/click_sound.mp3';
+import clickSoundSrc from '/src/assets/music/click_sound.mp3';
 
 const router = useRouter();
 const route = useRoute();
-const clickSound = new Audio(clickSoundSource);
+const clickSound = new Audio(clickSoundSrc);
 
 function redirectToSingleplayer() {
   playClickSound();
